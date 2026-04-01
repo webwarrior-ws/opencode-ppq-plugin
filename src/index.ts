4a508501542830cfafbd67f5747e382cc87c4730
webwarrior-ws <reg@webwarrior.ws>
2026-04-01 14:30:39 +0200

    Add PPQ.ai provider and load models

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,54 @@
 import type { Plugin } from '@opencode-ai/plugin';
 import path from 'path';
 
+interface PPQPricing {
+	input_per_1M_tokens: number;
+	output_per_1M_tokens: number;
+}
+
+interface PPQModel {
+	id: string;
+	name: string;
+	context_length: number;
+	pricing: PPQPricing;
+}
+
+interface PPQApiResponse {
+	data: PPQModel[];
+}
+
+async function fetchPPQModels() {
+	try {
+		//console.log("Fetching models from PPQ.ai...");
+		const response = await fetch("https://api.ppq.ai/v1/models");
+		const data = (await response.json()) as PPQApiResponse;
+
+		const models : { [key: string]: any } = {};
+
+		for (const model of data.data) {
+			models[model.id] = {
+				id: model.id,
+				name: model.name,
+				cost: {
+					input: model.pricing.input_per_1M_tokens,
+					output: model.pricing.output_per_1M_tokens
+				},
+        limit: {
+            context: model.context_length,
+            output: 4096 // ?
+        }
+        // modalities?
+			};
+		}
+
+		//console.log(`Fetched ${models.length} models from PPQ.ai`);
+		return models;
+	} catch (error) {
+		//console.error("Failed to fetch PPQ.ai models:", error);
+		return {};
+	}
+}
+
 export const PPQPlugin: Plugin = async ({ project, client, $, directory, worktree }) => {
   await client.app.log({
     body: {
@@ -23,7 +71,28 @@ export const PPQPlugin: Plugin = async ({ project, client, $, directory, worktre
       // Initialize the providers dictionary if it doesn't exist
       config.provider = config.provider ?? {};
 
-      // TODO: Add PPQ.ai provider
+      const modelsCacheFilePath = path.join(directory, ".opencode", "ppq-models-chache.json");
+
+      const models = await fetchPPQModels();
+
+      await client.app.log({
+        body: {
+          service: "ppq-plugin",
+          level: "info",
+          message: `${Object.keys(models).length} PPQ.ai models fetched`
+        },
+      });
+
+      const apiKey = process.env.PPQ_API_KEY;
+      config.provider["ppq"] = {
+        npm: "@ai-sdk/openai-compatible",
+        name: "PPQ.ai",
+        options: {
+          baseURL: "https://api.ppq.ai",
+          apiKey: apiKey
+        },
+        models: models
+      };
     },
   };
 };
